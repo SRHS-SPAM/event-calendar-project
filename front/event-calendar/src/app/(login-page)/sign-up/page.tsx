a57363f57d6f827f01c8e12a19bd1df6f89eb1ca
JangSeung <0sunghee122@gmail.com>
2024-12-15 20:26:57 +0900

    .

--- front/event-calendar/src/app/(login-page)/sign-up/page.tsx
+++ front/event-calendar/src/app/(login-page)/sign-up/page.tsx
@@ -1,5 +1,15 @@
 export default function SignUp() {
     return(
-        <h1>Login Page</h1>
+        <div className="w-full h-full">
+            <h1 className="h-40 w-full flex items-center justify-center text-5xl font-bold">
+                Login Page
+            </h1>
+            <div className="flex justify-center">
+                <div className="w-[30vw] h-[60vh] border-2 border-black rounded-xl shadow-2xl">
+                </div>
+            </div>  
+        </div>
+        
+        
     )
 }
