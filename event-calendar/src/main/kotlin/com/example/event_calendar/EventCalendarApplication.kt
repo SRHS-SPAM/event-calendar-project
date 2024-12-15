40bd440ab70f1f02c4a8dcabf02fbadc9796475e
andire120 <164429441+andire120@users.noreply.github.com>
2024-12-15 14:01:36 +0900

    커밋

---
+++ event-calendar/src/main/kotlin/com/example/event_calendar/EventCalendarApplication.kt
@@ -0,0 +1,11 @@
+package com.example.event_calendar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EventCalendarApplication
+
+fun main(args: Array<String>) {
+	runApplication<EventCalendarApplication>(*args)
+}
