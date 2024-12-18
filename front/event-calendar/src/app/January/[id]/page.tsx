72e3c1635044727654a6b8e0bdde4be870e78817
JangSeung <0sunghee122@gmail.com>
2024-12-18 15:43:56 +0900

    .

--- front/event-calendar/src/app/January/[id]/page.tsx
+++ front/event-calendar/src/app/January/[id]/page.tsx
@@ -1,44 +1,44 @@
-// "use client";
+"use client";
 
-// import { usePathname } from "next/navigation";
-// import { use } from "react";
+import { usePathname } from "next/navigation";
+import { use } from "react";
 
-// type Props = {
-//   params: Promise<{
-//     id: string;
-//   }>;
-// };
-
-// export default function Read({ params }: Props) {
-//   const pathname = usePathname();
-//   const Params = use(params); 
-
-
-//   return (
-//     <>
-//       <h1>January.{Params.id}</h1>
-//     </>
-//   );
-// }
-
-import { useRouter } from "next/router";
-
-const messages: Record<string, string> = {
-  "1": "안녕하세요 반가워요",
-  "2": "오랜만이에요",
+type Props = {
+  params: Promise<{
+    id: string;
+  }>;
 };
 
-export default function DynamicPage() {
-  const router = useRouter();
-  const { day } = router.query;
+const messages: Record<string, string> = {
+  "1": "지구 가족의 날",
+  "2": "몰라",
+};
 
-  const message = typeof day === "string" ? messages[day] || "페이지를 찾을 수 없습니다." : "";
+export default function Read({ params }: Props) {
+  const pathname = usePathname();
+  const Params = use(params); 
+  
+  let massage;
+
+  switch (Params.id) {
+    case "1":
+      massage = messages[1];
+      break;
+
+    case "2":
+      massage = messages[2];
+      break;
+  
+    default:
+      break;
+  }
+
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="text-center">
-        <h1 className="text-2xl font-bold text-gray-800">{message}</h1>
-      </div>
-    </div>
+    <>
+      <h1>1월 {Params.id}일</h1>
+      <h1>{massage}</h1>
+    </>
   );
 }
+
