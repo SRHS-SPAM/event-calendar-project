cd58e093f3d72090671e91e67fdb5250c71c258f
andire120 <164429441+andire120@users.noreply.github.com>
2025-01-28 19:31:12 +0900

    f

--- front/event-calendar/src/app/January/1/page.tsx
+++ front/event-calendar/src/app/January/1/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import axios from "axios";
-import { Black_Han_Sans } from "next/font/google";
+import { Playwrite_ID } from "next/font/google";
 import { cn } from "@/lib/utils";
 
 // API 기본 URL 설정 (백엔드 서버의 주소)
 const API_BASE_URL = "/api/users/";
 
-const PlaywriteAUNSW = Black_Han_Sans({ weight: "400" });
+const PlaywriteAUNSW = Playwrite_ID({ weight: "400" });
 
 // Axios 인스턴스 생성
 const apiClient = axios.create({
@@ -23,28 +23,48 @@ export default function J1() {
     <div className="h-[100vh] w-[100vw]">
       <div className="h-full w-full absolute inset-0 -z-10">
           <img
-            src="/win-bg.png"
+            src="/apple1.png"
             alt="Background"
             className="w-full h-full object-cover"
           />
       </div>
       <div className="h-full w-full flex justify-center items-center">
         <div className="pt-36">
-          
           <h1
             className={cn(
-              "h-40 w-full flex items-center justify-center text-9xl font-bold text-cyan-500",
+              "h-7 w-85 flex items-center justify-center text-8xl font-bold text-black-500 mb-[15%]",
               PlaywriteAUNSW.className
-            )}>사과 선물하기의 날</h1>
+            )}>Apple Gifting Day</h1>
             <h2
             className={cn(
-              "h-10 w-full flex items-center justify-center text-7xl text-cyan-500",
+              "w-full flex items-center justify-center text-6xl text-black-400 mb-[10%]",
               PlaywriteAUNSW.className
-            )}>1월 1일</h2>
+            )}>Jan. 1st</h2>
         </div>
-        <div className="h-[100%] w-[80%] absolute inset-0 -z-10 ml-[10%]">
+        <div className="h-[100%] w-[28%] absolute inset-0 -z-10 mr-[80%]">
             <img
-              src="/apple1.png"
+              src="/appletree.png" //이미지
+              alt="Background"
+              className="w-full h-full"
+            />
+        </div>
+        <div className="h-[50%] w-[28%] absolute inset-0 -z-10 ml-[70%] mt-[25%]">
+            <img
+              src="/a99le.png" //이미지
+              alt="Background"
+              className="w-full h-full"
+            />
+        </div>
+        <div className="h-[50%] w-[28%] absolute inset-0 -z-10 mb-[1%] mr-[30%]">
+            <img
+              src="/apple2.png" //이미지
+              alt="Background"
+              className="w-full h-full"
+            />
+        </div>
+        <div className="h-[30%] w-[25%] absolute inset-0 -z-10 ml-[7%] mt-[38%]">
+            <img
+              src="/bppple.png" //이미지
               alt="Background"
               className="w-full h-full"
             />
