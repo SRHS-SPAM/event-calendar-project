f7b2b81c86397a7a40eebffbf94cff31a4920b04
andire120 <164429441+andire120@users.noreply.github.com>
2025-02-27 20:07:22 +0900

    16

--- front/event-calendar/src/app/January/16/page.tsx
+++ front/event-calendar/src/app/January/16/page.tsx
@@ -25,7 +25,7 @@ export default function J1() {
     <div className="h-[100vh] w-[100vw]">
       <div className="h-full w-full absolute inset-0 -z-10 opacity-70">
           <img
-            src="/wcoback.jpg"
+            src="/noback.png"
             alt="Background"
             className="w-full h-full object-cover"
           />
@@ -36,7 +36,7 @@ export default function J1() {
           className={cn(
             "h-7 w-85 flex items-center justify-center text-7xl font-bold text-black-500 mb-[15%]",
             PlaywriteAUNSW.className
-          )}>National Welsh Corgi Day</h1>
+          )}>Nothing Day</h1>
           <h2
           className={cn(
             "w-full flex items-center justify-center text-5xl text-black-400 mb-[10%]",
@@ -46,28 +46,7 @@ export default function J1() {
           className={cn(
             "h-7 w-85 flex items-center justify-center text-6xl font-bold text-black-500 z-5 mt-[20%]",
             NanumBrushScript.className
-          )}>아무것도 아닌닌 날</p>
-        </div>
-        <div className="h-[50%] w-[30%] absolute inset-0 -z-10 mr-[80%]">
-            <img
-              src="/wellco2.png" //이미지
-              alt="Background"
-              className="w-full h-full"
-            />
-        </div> 
-        <div className="h-[57%] w-[54%] absolute inset-0 -z-55 mr-[8%] mt-[20%]">
-            <img
-              src="/wellco1.png" //이미지
-              alt="Background"
-              className="w-full h-full"
-            />
-        </div>
-        <div className="h-[60%] w-[40%] absolute inset-0 -z-1 ml-[60%] mt-[20.3%]">
-            <img
-              src="/wellco3.png" //이미지
-              alt="Background"
-              className="w-full h-full"
-            />
+          )}>아무것도 아닌 날</p>
         </div>
       </div>
     </div>
