64e125213b6d6b640cd90df750e9e333650e99da
JangSeung <0sunghee122@gmail.com>
2025-03-06 20:13:52 +0900

    .

--- front/event-calendar/src/app/page.tsx
+++ front/event-calendar/src/app/page.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>home</div>
+    <>
+      <div>home</div>
+      <Link href="/link" className="text-blue-500 font-bold hover:underline">이벤트 페이지 보러가기 →</Link>
+    </>
+    
+
+    
   );
 }
